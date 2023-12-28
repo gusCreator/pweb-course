56f34dae2c7caa79bfb5adeee3918528b79714ab
gusCreator <lgsc21211@gmail.com>
2023-12-28 17:00:11 +0000

    Creating the structure of the logic in calcular.pl

---
+++ lab08/cgi-bin/calcular.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+ 
+sub solvePar {
+
+}
+
+sub solveSum {
+
+}
+sub solveRes {
+
+}
+
+sub solveMul {
+
+}
+sub solveDiv {
+
+}
+sub recognize {
+
+}
