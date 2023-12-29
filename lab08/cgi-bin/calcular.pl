bb2a3900cfefd5f53dbe141fd7c80b6b6b9f857f
gusCreator <lgsc21211@gmail.com>
2023-12-29 14:31:59 +0000

    Implementing the method recognize for the separation of the elements in the ecuation. Also, the method recognize the letters in the ecuation

--- lab08/cgi-bin/calcular.pl
+++ lab08/cgi-bin/calcular.pl
@@ -4,7 +4,7 @@ use warnings;
 use CGI;
  
 sub solvePar {
-
+  
 }
 
 sub solveSum {
@@ -21,5 +21,12 @@ sub solveDiv {
 
 }
 sub recognize {
-
+  my $ec = $_[0];
+  if($ec =~ /[a-z]|[A-Z]/){
+    return "No es una expresión válida\n";
+  }else{
+    my $ec =~ s/ //g;
+    my $ec =~ s/-/+-/g;
+    my  @members = split('+', $ec);
+  }
 }
