4ec059a07df443f0a1d6e34acc00427a61603e53
gusCreator <lgsc21211@gmail.com>
2024-01-02 16:28:53 -0500

    I have created content class. This has the same design of the form. I changed to class becausa I will use in cgi.

--- lab09/cgi-bin/consult.pl
+++ lab09/cgi-bin/consult.pl
@@ -3,3 +3,29 @@ use strict;
 use warnings;
 use CGI;
  
+my $cgi = CGI->new;
+cgi->charset('UTF-8');
+print $cgi->header('text/html');
+print <<HTML;
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <title>Página de Búsqueda - Universidades licenciadas</title>
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="mytitle">
+        <b>Resultados de la búsqueda</b>
+      </div>
+      <div class="content">
+HTML
+
+print <<HTML;
+      </div>
+    </div>
+  </body>
+</html>
+HTML
