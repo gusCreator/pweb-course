32a5faa9f945992a12d9939911fb0800f6408905
gusCreator <lgsc21211@gmail.com>
2024-01-04 17:19:46 -0500

    I have created first CGI file. This contains some html structure

---
+++ lab10/cgi-bin/first.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+use Net::Adress::IP::Local;
+ 
+my $cgi = CGI->new;
+$cgi->charset('UTF-8');
+print $cgi->header('text/html; charset=UTF-8');
+print <<HTML;
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <title>Actor 5</title>
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="mytitle">
+        <b>El actor de ID: 5</b>
+      </div>
+      <div class="container">
+HTML
+my $user = "alumno";
+my $password = "pweb1";
+my $ip = Net::Adress::IP::Local->public_ipv4;
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.0.20";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+print <<HTML;
+      </div>
+      <div class="back">
+        <a href="../consult.html">Volver</a>
+      </div>
+    </div>
+  </body>
+</html>
+HTML
