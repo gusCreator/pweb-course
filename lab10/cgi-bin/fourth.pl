660d547966019ab72b6b7ba20601b9e86e2af90e
gusCreator <lgsc21211@gmail.com>
2024-01-05 22:51:18 +0000

    I have improved fourth cgi, i have added the table here, and i have fixed the use of cgi object (cgi->$cgi)

--- lab10/cgi-bin/fourth.pl
+++ lab10/cgi-bin/fourth.pl
@@ -22,23 +22,37 @@ print <<HTML;
       <div class="mytitle">
         <b>Pelis por año</b>
       </div>
-      <div class="container">
+      <div class="content">
 HTML
 my $user = "alumno";
 my $password = "pweb1";
 my $ip = Net::Address::IP::Local->public_ipv4;
 my $dsn = "DBI:MariaDB:database=pweb1;host=$ip";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+my $year = $cgi->param('year');
 
-my $year = cgi->param('year');
-my $sth = $dbh->prepare("SELECT * FROM Movie WHERE Year=?");
+print "<div class='mytable'>\n<table>\n<tr>\n";
+my $sth = $dbh->prepare("DESC Movie");
+$sth->execute();
+while(my @head = $sth->fetchrow_array){
+  print "<th>$head[0]</th>\n";
+}
+print "</tr>\n";
+
+$sth = $dbh->prepare("SELECT * FROM Movie WHERE Year=?");
 $sth->execute($year);
 while(my @row = $sth->fetchrow_array){
-  print "<p>@row</p>\n";
+  print "<tr>\n";
+  foreach my $data(@row){
+    print "<td>$data</td>\n";
+  }
+  print "</tr>\n";
 }
 $sth->finish;
 $dbh->disconnect;
 print <<HTML;
+      </table>
+      </div>
       </div>
       <div class="back">
         <a href="../consult.html">Volver</a>
