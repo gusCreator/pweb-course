656d6b065c2017e768a632b417c7fd650cbcb67a
gusCreator <lgsc21211@gmail.com>
2024-01-10 11:34:09 +0000

    I have created find method. This creates a map with the search paramethers for line

--- lab11/js/fetch.js
+++ lab11/js/fetch.js
@@ -6,14 +6,28 @@ function show(){
       }
       return response.text();
     })
-    .then(() => {
-      console.log("wait");
-      table();
+    .then(data => {
+      String(data);
+      table(data);
     })
     .catch(err => console.error("Error", err));
 }
-function table(){
+function table(data){
   const kind = document.getElementById("kind").value;
   const keyword = document.getElementById("keyword").value;
-  console.log(kind,keyword);
+  const lines = data.split("\n");
+  for(const line in lines){
+    const mydata = line.split("|");
+    const params = find(line, mydata);
+    const value = params.get(kind);
+  }
 }
+function find(line,mydata){
+  const uni = new Map([
+    ["name", mydata[1]],
+    ["period", mydata[4]],
+    ["localRegion", mydata[10]],
+    ["studyProgram", mydata[16]]
+  ]);
+  return uni;
+} 
