e7c1c08138640b9b7b7d01cfbda27d9f49cae3e9
gusCreator <lgsc21211@gmail.com>
2024-01-10 03:23:58 +0000

    I have tested to get values of kind and keyword

--- lab11/js/fetch.js
+++ lab11/js/fetch.js
@@ -1,14 +1,19 @@
-fetch('data.csv')
-.then(response => {
-  if(!response.ok){
-    throw new Error('No se concretó la solicitud');
-  }
-  return response.text();
-})
-.then(data => {
-  console.log("Datos obtenidos", data);
-})
-.catch(err => console.error("Error", err));
 function show(){
-  console.log("show...");
+  fetch('data.csv')
+    .then(response => {
+      if(!response.ok){
+        throw new Error('No se concretó la solicitud');
+      }
+      return response.text();
+    })
+    .then(() => {
+      console.log("wait");
+      table();
+    })
+    .catch(err => console.error("Error", err));
+}
+function table(){
+  const kind = document.getElementById("kind").value;
+  const keyword = document.getElementById("keyword").value;
+  console.log(kind,keyword);
 }
