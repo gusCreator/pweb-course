3bee3dfa5b397af9f95613880138ea03929196a3
gusCreator <lgsc21211@gmail.com>
2024-01-10 13:12:05 +0000

    I had many problems with my csv file, its codification was unknown, now I have created a csv file wih utf8 encoding

--- lab11/js/fetch.js
+++ lab11/js/fetch.js
@@ -1,13 +1,15 @@
 function show(){
-  fetch('data.csv')
+  fetch('data_utf8.csv')
     .then(response => {
       if(!response.ok){
         throw new Error('No se concretó la solicitud');
       }
-      return response.text();
+      const t = response.text();
+      console.log(t);
+      return t;
     })
-    .then(data => {
-      String(data);
+    .then(dat => {
+      const data = String(dat);
       table(data);
     })
     .catch(err => console.error("Error", err));
@@ -16,10 +18,22 @@ function table(data){
   const kind = document.getElementById("kind").value;
   const keyword = document.getElementById("keyword").value;
   const lines = data.split("\n");
-  for(const line in lines){
+  for(const line of lines){
     const mydata = line.split("|");
     const params = find(line, mydata);
     const value = params.get(kind);
+    if(!(typeof value === 'undefined') && value.includes(keyword.toUpperCase())){
+      const totable = [mydata[1], mydata[2], mydata[3],
+        mydata[4], mydata[7], mydata[8], 
+        mydata[10], mydata[11], mydata[12], 
+        mydata[15], mydata[16], mydata[17], 
+        mydata[18]];
+      for(const unis of totable){
+        const p = document.createElement("p");
+        p.textContent = unis;
+        document.body.appendChild(p);
+      }
+    }
   }
 }
 function find(line,mydata){
